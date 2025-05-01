b016632585ed2e8d43ad3c4d5f6b21758674ad2c
jay <917647288@qq.com>
2025-05-01 17:11:06 +0800

    删除重新迁移

--- test_event_dispatcher.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Just check if the class can be loaded without errors
-echo "Testing EventDispatcher class...\n";
-echo "Class exists: " . (class_exists('NativePHP\Think\EventDispatcher') ? 'Yes' : 'No') . "\n";
-echo "Interface exists: " . (interface_exists('NativePHP\Think\Contract\EventDispatcherContract') ? 'Yes' : 'No') . "\n";
-echo "Done.\n";
